e41b5796969f4a392e3105540f253d354dd42788
Joe Horacki <prince_937@hotmail.com>
2020-05-14 10:57:52 -0600

    Initial Commit

---
+++ Source/LuxShooter/Public/Characters/LuxPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/LuxCharacterBase.h"
+#include "LuxPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUXSHOOTER_API ALuxPlayerCharacter : public ALuxCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LuxShooter/Public/Game/LuxFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "LuxFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUXSHOOTER_API ULuxFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
