cb1985189c5c339207c4052071ba537ec9521b55
Joe Horacki <prince_937@hotmail.com>
2020-05-20 08:55:35 -0600

    Lecture 32. Building our own Variables - Part B

--- Plugins/RTSGrids/Source/RTSGrids/Public/GridCoords.h
+++ Plugins/RTSGrids/Source/RTSGrids/Public/GridCoords.h
@@ -10,19 +10,46 @@ struct FGridCoord
 {
 	GENERATED_BODY()
 
+	// GridLocation Column, or Y component
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Grids")
 	int Col;
 
+	// GridLocation Row, or X component
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Grids")
 	int Row;
 
+	// Default constructor (no initialization).
 	FORCEINLINE FGridCoord();
 
+	/** 
+	 * Constructor using initial values for each component.
+	 *
+	 * @param InColRow XY Coordinate
+	 */
 	FORCEINLINE FGridCoord(int InColRow);
 
+	/**
+	 * Constructor using initial values for each component.
+	 *
+	 * @param InCol Y Coordinate
+	 * @param InRow X Coordinate.
+	 */
 	FORCEINLINE FGridCoord(int InCol, int InRow);
 
+	/**
+	 * Constructor using initial values for each component.
+	 *
+	 * @param InCoord XY Coordinate
+	 */
 	FORCEINLINE FGridCoord(FVector2D InCoord);
+
+	FORCEINLINE bool operator==(FGridCoord& V) const;
+
+	FORCEINLINE FGridCoord operator*(float Scale) const;
+
+	FORCEINLINE FGridCoord operator*(int Scale) const;
+
+	FORCEINLINE FGridCoord operator*(FGridCoord& V) const;
 };
 
 FORCEINLINE FGridCoord::FGridCoord()
@@ -40,3 +67,29 @@ FORCEINLINE FGridCoord::FGridCoord(int InCol, int InRow)
 FORCEINLINE FGridCoord::FGridCoord(FVector2D InCoord)
 	: Col(InCoord.Y), Row(InCoord.X)
 {};
+
+FORCEINLINE bool FGridCoord::operator==(FGridCoord& V) const
+{
+	return Col == V.Col && Row == V.Row;
+}
+
+FORCEINLINE FGridCoord FGridCoord::operator*(float Scale) const
+{
+	return FGridCoord(Col * Scale, Row * Scale);
+
+}
+
+FORCEINLINE FGridCoord FGridCoord::operator*(int Scale) const
+{
+	return FGridCoord(Col * Scale, Row * Scale);
+}
+
+FORCEINLINE FGridCoord FGridCoord::operator*(FGridCoord& V) const
+{
+	return FGridCoord(Col * V.Col, Row * V.Row);
+}
+
+FORCEINLINE uint32 GetTypeHash(const FGridCoord& V)
+{
+	return FCrc::MemCrc_DEPRECATED(&V, sizeof(FGridCoord));
+}
